7c4fbe6230e6732e69a4c93dcbd96c8cae254fda
soomin <s2305@e-mirim.hs.kr>
2025-04-14 21:31:10 +0900

    [feat] user 회원가입 구현 (#7)
    엔드포인트 구현
    비밀번호 bcrypt로 암호화
    MySQL에 저장
    성공/실패 메시지 반환
    아이디, 이메일 중복 검사
    예외 처리

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,29 @@
 const db = require('../db');
 
+// 사용자 조회 userId
 exports.findUserByUserId = (userId, callback) => {
     const sql = 'SELECT * FROM users WHERE userId = ?';
-
     db.query(sql, [userId], callback);
+}
+
+// 사용자 조회 email
+exports.findUserByEmail = (email, callback) => {
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    db.query(sql, [email], callback);
+}
+
+// 사용자 추가
+exports.createUser = (name, userId, pass, email, callback) => {
+    const d = new Date();
+    const TIME_ZONE = 9 * 60 * 60 * 1000; // UTC+9 (한국 시간 기준)
+
+    const localTime = new Date(d.getTime() + TIME_ZONE);
+    const today = localTime.toISOString().slice(0, 19).replace('T', ' '); // 'YYYY-MM-DD HH:MM:SS'로
+
+    const sql = `INSERT INTO users(name, userId, pass, email, updatedAt, createdAt)
+                 VALUES(?, ?, ?, ?, ?, ?)`;
+
+    const values = [name, userId, pass, email, today, today];
+
+    db.query(sql, values, callback);
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 
 // 로그인 라우터
 router.post('/login', authController.loginUser);
+router.post('/join', authController.JoinUser);
 
 module.exports = router;
