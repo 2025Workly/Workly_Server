af928f51cabe850079feab06a16df6b36901f738
soomin <s2305@e-mirim.hs.kr>
2025-04-15 23:52:57 +0900

    [feat] user 탈퇴 구현 - 탈퇴 엔드포인트 - 사용자 인증, 토큰 확인 - MySQL에서 삭제 - 성공/오류 메시지 반환환

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,9 @@ const authController = require('../controllers/authController');
 
 // 로그인 라우터
 router.post('/login', authController.loginUser);
-router.post('/join', authController.JoinUser);
+// 회원가입 라우터
+router.post('/join', authController.joinUser);
+// 회원탈퇴 라우터
+router.delete('/', authController.deleteUser);
 
 module.exports = router;
