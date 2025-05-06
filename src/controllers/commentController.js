b1be69972f37187cbac41f75e38c1c659de446c1
soomin <s2305@e-mirim.hs.kr>
2025-05-07 00:47:37 +0900

    [feat] 댓글 목록 api 구현 (#39)

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -43,3 +43,44 @@ exports.addComment = async (req, res) => {
         return res.status(500).json({ message: '서버 오류' });
     }
 };
+
+// 댓글 삭제
+exports.getComment = async (req, res) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: '토큰이 없습니다.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.userId;
+
+        const user = await User.findOne({ where: { userId } });
+        if (!user) {
+        return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
+        }
+
+        // 게시글 ID 가져오기
+        const boardId = req.params.id;
+
+        // 게시글 검색
+        const board = await Board.findOne({ where: { id: boardId } });
+
+        if (!board) {
+            return res.status(404).json({ message: '게시글을 찾을 수 없습니다.' });
+        }
+
+        const comments = await Comment.findAll({
+            where: {
+                boardId: boardId
+            },
+            order: [['createdAt', 'ASC']] // 최신순
+        });
+
+        return res.status(200).json({ comments });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: '서버 오류' });
+    }
+};
--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const commentController = require('../controllers/commentController');
 
 router.post('/:id', commentController.addComment);
+router.get('/:id', commentController.getComment);
 
 module.exports = router;
