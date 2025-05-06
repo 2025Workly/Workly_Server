ce46580a6910230d902453f39787eed83031b1e9
soomin <s2305@e-mirim.hs.kr>
2025-05-07 04:24:16 +0900

    [feat] 팁 목록 api 구현 (#52)

--- src/controllers/tipController.js
+++ src/controllers/tipController.js
@@ -39,3 +39,50 @@ exports.addTip = async (req, res) => {
         return res.status(500).json({ message: '서버 오류' });
     }
 }
+
+// 팁 목록
+exports.getTip = async (req, res) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if(!token) {
+        return res.status(401).json({ message: '토큰이 없습니다.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.userId;
+
+        const user = await User.findOne({ where: { userId } });
+        if (!user) {
+        return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
+        }
+
+        // 카테고리 가져오기
+        const category = req.params.id;
+
+        if (!category) {
+            return res.status(404).json({ message: '카테고리가 없습니다.' });
+        }
+        let tips = [];
+
+        // 카테고리가 전체일 경우 전부 가져오기
+        if(category === "전체") {
+            tips = await Tip.findAll({
+                order: [['createdAt', 'DESC']] // 최신순
+            });
+        }
+        else {
+            tips = await Tip.findAll({
+                where: {
+                    category: category
+                },
+                order: [['createdAt', 'DESC']] // 최신순
+            });
+        }
+
+        return res.status(200).json({ tips });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: '서버 오류' });
+    }
+}
--- src/routes/tipRoutes.js
+++ src/routes/tipRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const tipController = require('../controllers/tipController');
 
 router.post('/', tipController.addTip);
+router.get('/:id', tipController.getTip);
 
 module.exports = router;
