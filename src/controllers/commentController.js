338dbb88a6c1b8ae28a2d15acd79f3f9bb630464
soomin <s2305@e-mirim.hs.kr>
2025-05-07 00:53:30 +0900

    [feat] 댓글 삭제 api 구현 (#40)

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -84,3 +84,39 @@ exports.getComment = async (req, res) => {
         return res.status(500).json({ message: '서버 오류' });
     }
 };
+
+// 댓글 삭제
+exports.deleteComment = async (req, res) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: '토큰이 없습니다.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.userId;
+
+        const user = await User.findOne({ where: { userId } });
+        if (!user) {
+        return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
+        }
+
+        // 댓글 ID 가져오기
+        const commentId = req.params.id;
+
+        // 댓글 검색
+        const comment = await Comment.findOne({ where: { id: commentId } });
+
+        if (!comment) {
+            return res.status(404).json({ message: '댓글을 찾을 수 없습니다.' });
+        }
+
+        await comment.destroy();
+
+        return res.status(200).json({ message: '댓글이 삭제되었습니다.' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: '서버 오류' });
+    }
+};
--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -4,5 +4,6 @@ const commentController = require('../controllers/commentController');
 
 router.post('/:id', commentController.addComment);
 router.get('/:id', commentController.getComment);
+router.delete('/:id', commentController.deleteComment);
 
 module.exports = router;
