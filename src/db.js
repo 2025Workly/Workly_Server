b4d30701017bee4d5e62e43b6fb99b692e5ca8b3
soomin <s2305@e-mirim.hs.kr>
2025-06-14 22:12:31 +0900

    [fix] dotenv 코드 최상단으로 변경

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
-const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, 'config/.env') });
+const path = require('path');
 
 const { Sequelize } = require('sequelize');
 
