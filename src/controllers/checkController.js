9548c0b71d36a09c9d3adb0b59f95c335dbf5f12
soomin <s2305@e-mirim.hs.kr>
2025-06-15 22:11:55 +0900

    [feat] 월별 체크리스트 목록 api 구현

--- src/controllers/checkController.js
+++ src/controllers/checkController.js
@@ -1,6 +1,7 @@
 const Check = require('../models/checkModel');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const sequelize = require('../db');
 
 // 체크리스트 추가
 exports.addCheck = async (req, res) => {
@@ -40,6 +41,49 @@ exports.addCheck = async (req, res) => {
         });
 
         return res.status(200).json({ message: '리스트가 등록되었습니다.' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: '서버 오류' });
+    }
+};
+
+// 월별 체크리스트 조회
+exports.getMonthCheckList = async (req, res) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    const { year, month } = req.query;
+
+    if (!token) {
+        return res.status(401).json({ message: '토큰이 없습니다.' });
+    }
+
+    if (!year || !month) {
+        return res.status(400).json({ message: '연도와 월을 입력해주세요.' });
+    }
+
+    const yearMonth = `${year}-${String(month).padStart(2, '0')}`;
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.userId;
+
+        const user = await User.findOne({ where: { userId } });
+        if (!user) {
+        return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
+        }
+
+        const checkLists = await Check.findAll({
+        where: {
+            userId,
+            date: sequelize.where(
+            sequelize.fn('DATE_FORMAT', sequelize.col('date'), '%Y-%m'),
+            yearMonth
+            ),
+        },
+        });
+
+        const checkListDays = checkLists.map(c => c.date);
+
+        return res.status(200).json({ checkListDays });
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: '서버 오류' });
--- src/routes/checkRoutes.js
+++ src/routes/checkRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const checkController = require('../controllers/checkController');
 
 router.post('/', checkController.addCheck);
+router.get('/month', checkController.getMonthCheckList);
 router.get('/', checkController.getCheckList);
 router.patch('/:checkId', checkController.toggleCheck);
 router.delete('/:checkId', checkController.deleteCheck);
